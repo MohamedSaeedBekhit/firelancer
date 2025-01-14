f35813e4fc0b7744b49567fce53f340ea2706d8c
nostva nostva <nostva9000@gmail.com>
2025-01-14 12:03:18 +0200

    feat(admin-ui): initialize Angular application structure with basic configuration and styles

---
+++ packages/admin-ui/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+
+@Component({
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+})
+export class AppComponent {
+    title = 'admin-ui';
+
+    constructor(private primeng: PrimeNG) {}
+
+    ngOnInit() {
+        this.primeng.ripple.set(true);
+    }
+}
